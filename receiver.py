e6b9200f2fb670f1a4336369f8037ade8353612b
PavelM87 <58486727+PavelM87@users.noreply.github.com>
2021-03-10 19:48:20 +0300

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -11,7 +11,10 @@ def bx24(request):
     data = request.POST
     key = data['auth[application_token]']             # ключ исходящего хука
     lead_id = data['data[FIELDS][ID]']                # id лида, который был обновлен
-    response = requests.get('https://<Your_bitrix24_domain_name>/rest/<user_id>/<autorization_code>/crm.lead.get/', {'id': lead_id}) # исходящий хук к обновленному лиду
+    response = requests.get(
+        'https://<Your_bitrix24_domain_name>/rest/<user_id>/<autorization_code>/crm.lead.get/',
+        {'id': lead_id}
+    )                                                 # исходящий хук к обновленному лиду
     if key=='**********' or key=='**********':        # ключи хуков
         print()
         print(response.json()['result']['STATUS_ID']) # статус обновленного лида
